40da8677a50075e5ff82bf1844ae96a97b263a1a
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2023-12-17 12:42:50 -0500

    Added records 8-1

--- src/data/data_pho.js
+++ src/data/data_pho.js
@@ -1,5 +1,10 @@
 const pho1_data = [
     {
+        player: 'xD3viLz',
+        time: 677,
+        src: 'https://www.youtube.com/watch?v=oObEDupB8Ys&t=2s',
+    },
+    {
         player: '玉米大战僵尸',
         time: 1288,
         src: 'https://www.bilibili.com/video/BV1Jj411s7CX/?spm_id_from=333.337',
