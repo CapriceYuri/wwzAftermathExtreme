0ba228021732cfadd87c6c787a197562a9da3f49
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2024-01-23 11:09:31 -0500

    NR K3

--- src/data/data_kam.js
+++ src/data/data_kam.js
@@ -4,21 +4,18 @@ const kam1_data = [
         time: 1020,
         src: 'https://www.bilibili.com/video/BV1Dk4y157Jr/?spm_id_from=333.788',
         team: 'solo',
-        comp: ['D'],
     },
     {
         player: 'TW.HK.MY',
         time: 694,
         src: 'https://www.youtube.com/watch?v=ORUamv_6_cs',
         member: ['ABA', '楊楊', '龍蝦', '原神高手魚片'],
-        comp: ['D', 'D', 'V', 'F'],
     },
     {
         player: 'xD3viLz',
         time: 684,
         src: 'https://www.youtube.com/watch?v=VgNPKUT59w4',
         member: ['xD3viLz', 'bot', 'bot', 'bot'],
-        comp: ['D', 'E', 'D', 'G'],
         date: '3/20/2023'
     },
 ]
@@ -29,21 +26,18 @@ const kam2_data = [
         time: 1077,
         src: 'https://www.bilibili.com/video/BV1iu411V7rD/?spm_id_from=333.788',
         team: 'solo',
-        comp: ['D'],
     },
     {
         player: 'おやじ日本人',
         time: 660,
         src: 'https://www.youtube.com/watch?v=4UcrSqiJdyU',
-        member: ['おやじ日本人', 'bot'],
-        comp: ['D', 'E'],
+        member: ['おやじ日本人', 'BadOssan'],
     },
     {
         player: 'xD3viLz',
         time: 647,
         src: 'https://www.youtube.com/watch?v=_iyMyGoEkMM',
         member: ['xD3viLz', 'bot', 'bot', 'bot'],
-        comp: ['D', 'D', 'E', 'G'],
         date: '3/20/2023'
     },
 ]
@@ -54,21 +48,19 @@ const kam3_data = [
         time: 1129,
         src: 'https://www.bilibili.com/video/BV1LF411X7AG/?spm_id_from=333.788',
         team: 'solo',
-        comp: ['D'],
     },
     {
         player: 'おやじ日本人',
-        time: 937,
-        src: 'https://www.youtube.com/watch?v=qoN0ajbOk-U',
-        member: ['おやじ日本人', 'bot'],
-        comp: ['D', 'E'],
+        time: 871,
+        src: 'https://www.youtube.com/watch?v=QAW-jMM9NIQ',
+        member: ['おやじ日本人', 'BadOssan'],
+        date: '1/20/2024'
     },
     {
         player: 'xD3viLz',
         time: 922,
         src: 'https://www.youtube.com/watch?v=I1bbmp7QtAI',
         member: ['xD3viLz', 'bot', 'bot', 'bot'],
-        comp: ['D', 'D', 'E', 'G'],
         date: '3/20/2023'
     },
 ]
